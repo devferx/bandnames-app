6c11abf5c6b8b19154b5510f5dcc229652056504
devferx <ferkki@hotmail.com>
2022-04-29 14:53:12 -0400

    useSocket - CustomHook

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
 import { useEffect, useState } from "react";
-import { connect } from "socket.io-client";
 
 import { BandAdd } from "./components/BandAdd";
 import { BandList } from "./components/BandList";
+import { useSocket } from "./hooks/useSocket";
+
 import { Band } from "./interfaces/Band";
 
-const connectSocketServer = () => {
-  const socket = connect("http://localhost:8082", {
-    transports: ["websocket"],
-  });
-  return socket;
-};
-
 function App() {
-  const [socket] = useState(connectSocketServer());
-  const [online, setOnline] = useState(false);
+  const { socket, online } = useSocket("http://localhost:8082");
   const [bands, setBands] = useState<Band[]>([]);
-
-  useEffect(() => {
-    setOnline(socket.connected);
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("connect", () => {
-      setOnline(true);
-    });
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("disconnect", () => {
-      setOnline(false);
-    });
-  }, [socket]);
 
   useEffect(() => {
     socket.on("current-bands", (bands: Band[]) => {
