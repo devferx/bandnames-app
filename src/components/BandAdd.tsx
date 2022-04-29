254fd8f83466125e38c1242b06c0c8c60d00ca65
devferx <ferkki@hotmail.com>
2022-04-29 11:25:57 -0400

    Crear aplicación de React y los primeros componentes

---
+++ src/components/BandAdd.tsx
@@ -0,0 +1,14 @@
+export const BandAdd = () => {
+  return (
+    <>
+      <h3>Agregar Banda</h3>
+      <form>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Nuevo nombre de banda"
+        />
+      </form>
+    </>
+  );
+};
